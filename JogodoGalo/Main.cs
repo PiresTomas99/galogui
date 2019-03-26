b1576cdde54ef5f0c2da60cd27a4c1943d372904
TomasPires <pirestomas99@gmail.com>
2019-03-26 17:22:51 +0000

    trabalho

---
+++ JogodoGalo/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogodoGalo
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void novoJogoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 Galo = new Form1();
+
+            Galo.MdiParent = this;
+
+            Galo.Show();
+
+
+
+        }
+    }
+}
